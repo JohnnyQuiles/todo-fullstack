0febc1aa9ef4dfdef2e5e4b5159a13725c165ab0
Johnny Quiles <79813390+JohnnyQuiles@users.noreply.github.com>
2022-04-20 18:00:33 -0400

    creating task, need to display

--- src/Pages/Create-task.js
+++ src/Pages/Create-task.js
@@ -1,22 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../Components/NavBar';
 
 
 function CreateTasks() {
-    const [todo, setTodo] = useState("");
-
     return (
         <div className="create-tasks-page">
             <NavBar />
-            <h1>Create-Tasks Page</h1>
-            <div className='task-table'>
-                <input type="text" value={todo} placeholder='create task' onChange={(event) => {
-                    setTodo(event.target.value);
+            <h1>Create-Tasks</h1>
+            <div className='task'>
+                <label>Title:</label>
+                <input type="text" placeholder='create task' onChange={() => {
                     console.log("typing");
                 }} />
+                <div className='description'>
+                    <label>Description:</label>
+                    <input type='text'></input>
+                </div>
+                <div className='priority'>
+                    <label>Priority:</label>
+                    <select>
+                        <option value="Low">Low</option>
+                        <option value="Medium">Medium</option>
+                        <option value="High">High</option>
+                        <option value="Critical">Critical</option>
+                    </select>
+                </div>
+                <div className="complete">
+                    <label>Complete</label>
+                    <input type="checkbox" name="" />
+                </div>
                 <button onClick={async () => {
-                    console.log(todo);
                 }}>Create-tasks</button>
                 <Outlet />
             </div>
